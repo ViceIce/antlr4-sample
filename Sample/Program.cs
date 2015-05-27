282f2af0bbe7d794db6d331e43ed2faaa062be80
Michael Kriese <viceice@users.noreply.github.com>
2015-05-27 19:57:02 +0200

    Updated sample.

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Sample
+﻿using Newtonsoft.Json.Linq;
+
+namespace Sample
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var s = new Antlr4.Runtime.AntlrInputStream("Test");
-
+            //var s = new Antlr4.Runtime.AntlrInputStream("Test");
+            var j = new JObject();
         }
     }
 }
